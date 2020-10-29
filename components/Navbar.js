d3626f92da7f400de52d57f53598a8c5fe3d0ba0
Eren Doru <erendoru@hotmail.com>
2020-10-29 13:19:43 +0300

    Navbar Component
    A title, login and logout buttons are here

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,25 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar({ user }) {
     return (
         <nav className='flex justify-between items-center py-4'>
             <p className='text-2xl font-bold text-grey-800'>My Todos</p>
 
             <div className="flex">
-                <a href='/api/logout' className='rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4'>
-                    Logout
+                {
+                    user &&
+
+                    <a href='/api/logout' className='rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4'>
+                        Logout
                     </a>
-                <a href='/api/login' className='rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 '>
-                    Login
+                }
+                {
+                    !user &&
+
+                    <a href='/api/login' className='rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 '>
+                        Login
                     </a>
+                }
             </div>
         </nav>
     )
